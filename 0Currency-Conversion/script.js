6becd630757c8f313dfb2eec5ccad97d06b16e9f
GIZEM KARADAGOGLU <80019972+gizemkk@users.noreply.github.com>
2021-08-08 23:53:50 -0700

    Add files via upload

---
+++ 0Currency-Conversion/script.js
@@ -0,0 +1,34 @@
+
+
+const value=1000;
+
+function convertToUSD (){
+ const result = value * 1.4;
+ return result
+
+}
+
+console.log(convertToUSD(1000));
+
+function convertToBRL(){
+    const result = (value*5.7) * .99;
+    return result
+
+}
+console.log(convertToBRL(1000));
+
+function convertToAll(currency, value){
+    if(currency==='USD'){
+        return convertToUSD(value);
+     
+    }
+    else if(currency==='BRL'){
+        return convertToBRL(value)
+      
+    }
+else{
+  return 'enter current currency'
+}
+}
+console.log(convertToAll('USD',1000));
+
